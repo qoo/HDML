0dc0bd406a5e6112b5c33fc6ab1887a74817c785
chenghung_yeh <chenghung_yeh@apple.com>
2019-10-10 15:55:47 -0700

    try

--- lib/evaluation_icon.py
+++ lib/evaluation_icon.py
@@ -248,10 +248,12 @@ def Evaluation_icon(stream_test, image_mean, sess, x_raw, label_raw, is_Training
 def Evaluation_icon2(Evaluation_icon2, image_mean, sess, x_raw, label_raw, is_Training, embedding, num_class, neighb):
     y_batches = []
     c_batches = []
-    images, labels = Evaluation_icon2
+    images_, labels_ = Evaluation_icon2
+    images = images_[0]
+    labels = labels_[0]
     for idx, image in enumerate(tqdm(images)):
 
-        x_batch_data, c_batch_data = image, labels[idx]
+        x_batch_data, c_batch_data = [image], labels[idx]
         x_batch_data = np.transpose(x_batch_data[:, [2,1,0], :, :], (0, 2, 3, 1))
         x_batch_data = x_batch_data-image_mean
         y_batch = sess.run([tf.nn.l2_normalize(embedding, dim=1)],
