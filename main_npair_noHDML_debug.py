1193619365eb2dc306c4cac4d59167cbec0cb9c2
chenghung_yeh <chenghung_yeh@apple.com>
2019-10-17 10:17:31 -0700

    debug

--- main_npair_noHDML_debug.py
+++ main_npair_noHDML_debug.py
@@ -223,7 +223,8 @@ def main(_):
                     for batch in tqdm(copy.copy(stream_test.get_epoch_iterator())):
                         x_batch_data, c_batch_data = batch
                         print(x_batch_data.shape)
-                        print(c_batch_data)
+                        print(x_batch_data[0])
+                        print(c_batch_data[0])
 
 
                     # Summary
